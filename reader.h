55773b387817a09a65dd0773995b69925e756cb8
GabrielAntunes34 <antunes.gabriel.345@gmail.com>
2024-04-03 11:02:59 -0300

    Novo .h
    Funções iniciais do .h definidas

--- reader.h
+++ reader.h
@@ -1,10 +1,22 @@
 #ifndef READER_H
     #define READER_H
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
 
     typedef struct cabecalho CABECALHO;
     typedef struct registro REGISTRO;
 
-    reader_create();
-    reader_read();
+    // Cria o aqruivo com os registros do csv. recebe os nomes do arquivo de
+    // leitura e do de sáida, e retorna um bool para o sucesso da operação
+    bool reader_create_table(char *csv, char *binario);
+
+    // Imprime todos o registros do arquivo binário.
+    bool reader_select_from(char *binario);
+
+    // Imprime todos os registros do arquivo segundo uma quantidade qntd
+    // de buscas com condições próprias
+    bool reader_select_where(char * binario, int qntd);
+
 
 #endif
